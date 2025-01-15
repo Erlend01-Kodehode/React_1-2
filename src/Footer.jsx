db83724ad11093f8f0b14643168c8b9397604cf5
Erlend01-Kodehode <erlend01.kodehode@gmail.com>
2025-01-15 14:43:26 +0100

    pretty

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,11 @@ function Footer() {
     <div className="FooterDiv">
       <p>
         Apoligies for the change of menu, unfortunately, our previous methods of
-        acquiring tax breaks backfired and the government wants backpay.
+        acquiring tax breaks backfired and the government wants backpay.*
+      </p>
+      <p>
+        *Listing lembas bread multiple times is not a valid tax break
+        apparently.
       </p>
     </div>
   );
